418ffd55590fda259cf8f99e29ae34e38778beaa
ramdroid <ramdroid@mailbox.org>
2025-02-27 08:52:28 +0100

    fixed chest parsing, added start button

--- chests.py
+++ chests.py
@@ -1,9 +1,10 @@
 import sys
+import time
 
 # pip install pyqt5
 from PyQt5.QtGui import QPainter, QPen
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5.QtCore import Qt, pyqtSlot
 
 # pip install screeninfo
 from screeninfo import get_monitors
@@ -18,21 +19,15 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tess
 # pip install pyautogui
 import pyautogui
 
+
 class Chest:
 
     player = ''
     source = ''
     name = ''
-    type = ''
-    level = ''
-
-    def detailed_source(self):
-        s = self.source
-        if len(self.type) > 0:
-            s = self.type + ' ' + s
-        if len(self.level) > 0:
-            s += ' ' + self.level
-        return s
+    
+    def __str__(self):
+        return f'{self.player}: {self.source}'
     
     def __iter__(self):
         return iter((self.player, self.source, self.name))
@@ -40,8 +35,10 @@ class Chest:
     def valid(self):
         return len(self.name) > 0 and len(self.player) > 0 and len(self.source) > 0
 
+
 class ChestException(Exception):
     pass
+
 
 class OCRWindow(QMainWindow):
     
@@ -51,18 +48,35 @@ class OCRWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
-
-    def initUI(self):
         for m in get_monitors():
             print(str(m))
             if m.is_primary:
                 self.setGeometry(m.x, m.y, m.width, m.height)
 
-        self.setStyleSheet("background:transparent")
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.setWindowFlags(Qt.FramelessWindowHint)
+
+        button = QPushButton('Start chest tracker', self)
+        button.move(1300, 900)
+        button.clicked.connect(self.on_start)
+
         self.show()
+
+    @pyqtSlot()
+    def on_start(self):
+        total_chests = []
+        while (True):    
+            chests = self.grab()
+            if len(chests) > 0:
+                total_chests.extend(chests)
+                self.next()
+            else:
+                break
+
+            #break
+
+        export = Export()
+        export.csv(total_chests)
 
     def paintEvent(self, e):
         qp = QPainter()
@@ -91,35 +105,34 @@ class OCRWindow(QMainWindow):
     def next(self):
         for i in range(4):
             pyautogui.click(self.BUTTON[0] + (self.BUTTON[2] / 2), self.BUTTON[1] + (self.BUTTON[3] / 2))
+            time.sleep(0.5)
 
     def parse(self, text_lines):
         chests = []
         chest = Chest()
         for line in text_lines:
             if len(line) == 0:
-                if chest.valid():
-                    chests.append(chest)
-                    chest = Chest()
                 continue
             if len(chest.name) == 0:
                 chest.name = line
             elif len(chest.player) == 0:
                 if not line.startswith('From'):
                     raise ChestException()
-                chest.player = line.split(' ')[1]
+                s = line.split(' ')
+                s.pop(0)
+                chest.player = ' '.join(s)
             elif len(chest.source) == 0:
                 if not line.startswith('Source'):
                     raise ChestException()
                 s = line.split(' ')
-                if len(s) == 2:
-                    dummy, chest.source = s
-                elif len(s) == 4:
-                    dummy1, dummy2, chest.level, chest.source = s
-                elif len(s) == 5:
-                    dummy1, dummy2, chest.level, chest.type, chest.source = s
-                else:
-                    raise ChestException()
+                s.pop(0)
+                chest.source = ' '.join(s)
+            if chest.valid():
+                chests.append(chest)
+                print(chest)
+                chest = Chest()
         return chests
+
 
 class Export:
 
@@ -134,9 +147,8 @@ class Export:
             if not chest.player in self.players:
                 self.players.append(chest.player)
 
-            source = chest.detailed_source()
-            if not source in self.sources:
-                self.sources.append(source)
+            if not chest.source in self.sources:
+                self.sources.append(chest.source)
             
             if not chest.player in self.player_chests:
                 self.player_chests[chest.player] = []
@@ -153,7 +165,7 @@ class Export:
                         continue
                     counter = 0
                     for chest in self.player_chests[player]:
-                        if chest.detailed_source() == source:
+                        if chest.source == source:
                             counter += 1
                     sources.append(str(counter))
                 line = SEP.join(sources) + EOL
@@ -164,19 +176,4 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ocr = OCRWindow()
-
-    total_chests = []
-    while (True):    
-        chests = ocr.grab()
-        if len(chests) > 0:
-            total_chests.extend(chests)
-            #ocr.next()
-        else:
-            break
-
-        break
-
-    export = Export()
-    export.csv(total_chests)
-    
     sys.exit(app.exec_())
